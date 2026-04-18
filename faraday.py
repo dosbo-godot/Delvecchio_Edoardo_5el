711657a18c9b3a8a7926ac0fb487ea23b32cce9e
dosbo-godot <3d04rd0.D3lv3cchi0@gmail.com>
2026-04-19 00:10:10 +0200

    sistemato galvanometro con due implemetazioni (da 0 a 6 e da -3 a +3); create grafiche per 1' esperimento, preparati due stadi dell'interruttore. Satbilita struttura a ciclo .after() per tutti esperimenti, manca solo costruirli e mettere in relazione widgets slides con essi.

--- faraday.py
+++ faraday.py
@@ -6,6 +6,7 @@ import math
 
 GREY = '#e8e8e8'
 LARGHEZZA_LATERALE = 300
+PI = 3.1415
 
 class GestoreCanvas:
     def __init__(self):
@@ -14,16 +15,19 @@ class GestoreCanvas:
         # PARAMETRI GENERICI
         self.delta_potenziale = 0
         self.resistenza = 0
-        self.corrente = 0
+        self.corrente_potenziale = 0
+        self.corrente = -100
         self.corrente_max = 100
         self.corrente_indotta = 0
+        self.fps = 10
+        self.delta = 1000//self.fps
         # PARAMETRI 1°
         # PARAMETRI 2°
         # PARAMETRI 3°
         # PARAMETRI 4°
         # PARAMETRI 5°
 
-
+ 
     def disegna(self):
         x = random.randint(0, 400)
         self.canvas.create_oval(x+20, x+20,x-20,x-20, fill='red')
@@ -40,17 +44,17 @@ class GestoreCanvas:
         
         XCENTRO = larghezza-100
         YCENTRO = 146
-
-        #canvasGalvanometro = tk.Canvas()
-        self.img = tk.PhotoImage(file='galvanometro.gif')
+        #RAD_INIZIO = 0.66 + PI
+        RAD_INIZIO = PI*(3/2)
+        self.img = tk.PhotoImage(file='galvanometroMK2.gif')
         self.canvas.create_image(larghezza-(LARGHEZZA_GALVANOMETRO//2) - PADX, ALTEZZA_GALVANOMETRO//2 + PADY, image = self.img)
         
-        alfa = 0.921 + ((1.57*self.corrente)/self.corrente_max)
-        x = math.sin(alfa)*RAGGIO
-        y = math.cos(alfa)*RAGGIO
+        alfa = (RAD_INIZIO + ((0.91*self.corrente)/self.corrente_max))
+
+        x = math.cos(alfa)*RAGGIO
+        y = math.sin(alfa)*RAGGIO
         
-        self.canvas.create_line(XCENTRO, YCENTRO, larghezza-x, y, fill='red', width=4)
-        print(larghezza-100, larghezza-190)
+        self.canvas.create_line(XCENTRO, YCENTRO, XCENTRO+x, YCENTRO+y, fill='red', width=4)
 
 
     def disegnaPrimo(self):
@@ -60,30 +64,45 @@ class GestoreCanvas:
         self.canvas.create_oval(x+20, x+20,x-20,x-20, fill='grey')
         
         self.disegnaGalvanometro()
+        if gestore_dialogo.esperimento == '1° Esperimento':
+            self.canvas.after(1000//self.fps, self.disegnaPrimo)
+        else: return 0
 
     def disegnaSecondo(self):
         self.canvas.delete('all')
 
         x = random.randint(0, 400)
         self.canvas.create_oval(x+20, x+20,x-20,x-20, fill='yellow')
+        if gestore_dialogo.esperimento == '2° Esperimento':
+            self.canvas.after(self.delta, self.disegnaSecondo)
+        else: return 0
 
     def disegnaTerzo(self):
         self.canvas.delete('all')
 
         x = random.randint(0, 400)
         self.canvas.create_oval(x+20, x+20,x-20,x-20, fill='blue')
+        if gestore_dialogo.esperimento == '3° Esperimento':
+            self.canvas.after(self.delta, self.disegnaTerzo)
+        else: return 0
 
     def disegnaQuarto(self):
         self.canvas.delete('all')
 
         x = random.randint(0, 400)
         self.canvas.create_oval(x+20, x+20,x-20,x-20, fill='black')
+        if gestore_dialogo.esperimento == '4° Esperimento':
+            self.canvas.after(self.delta, self.disegnaQuarto)
+        else: return 0
 
     def disegnaQuinto(self):
         self.canvas.delete('all')
 
         x = random.randint(0, 400)
         self.canvas.create_oval(x+20, x+20,x-20,x-20, fill='green')
+        if gestore_dialogo.esperimento == '5° Esperimento':
+            self.canvas.after(self.delta, self.disegnaQuinto)
+        else: return 0
 
 class GestoreDialogo:
     def __init__(self) -> None:
@@ -94,7 +113,6 @@ class GestoreDialogo:
         self.diapositive : list[dict[str:str]]= self.config_dialogo[self.esperimento]
     
     def diapositivaSucc(self) -> None:
-        gestore_canvas.corrente += 5
         if self.index_diapositiva < len(self.diapositive)-1:
             self.index_diapositiva += 1
             self.aggiornaContoDiapositiva()
@@ -104,7 +122,6 @@ class GestoreDialogo:
 
     
     def diapositivaPrec(self) -> None:
-        gestore_canvas.corrente -= 5
         if self.index_diapositiva != 0:
             self.index_diapositiva -= 1
             self.aggiornaContoDiapositiva()
